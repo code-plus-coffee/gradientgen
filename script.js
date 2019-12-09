a6b8e335a910ba3ac74b56ea68eff50964a67b44
Code Plus Coffee <44864604+code-plus-coffee@users.noreply.github.com>
2019-12-09 09:33:04 +0530

    Initial CSS property. #09120931

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // Variables
-var cssProperty = document.getElementById("prop_dis");
-var color1 = document.getElementById("color1");
-var color2 = document.getElementById("color2");
+var cssProperty = document.getElementById("css-prop-display");
+var color1 = document.getElementById("c1");
+var color2 = document.getElementById("c2");
 var body = document.getElementById("body");
+var bg = getComputedStyle(body)
+
+cssProperty.textContent = bg.backgroundImage + ";";
 
 // Function to display the corresponding CSS property
 function showProperty() {
@@ -12,10 +15,10 @@ function showProperty() {
 // Function to set gradient
 function setGradient() {
     body.style.background = "linear-gradient(to right, "
-    + color1.value
-    + ", "
-    + color2.value
-    + ")";
+        + color1.value
+        + ", "
+        + color2.value
+        + ")";
 
     showProperty();
 }
