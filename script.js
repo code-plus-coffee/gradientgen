84d41e29115ee15e0fda4ef6b35a6bcc93b24d95
Code-Plus-Coffee <m416a@protonmail.com>
2019-12-09 16:21:56 +0530

    Added random gradient generating feature. #09121621

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Variables
+var randomIcon = document.getElementsByClassName("random-icon")[0];
 var cssProperty = document.getElementById("css-prop-display");
 var color1 = document.getElementById("c1");
 var color2 = document.getElementById("c2");
@@ -23,6 +24,31 @@ function setGradient() {
     showProperty();
 }
 
+/* Function to generate random gradients 
+    https://stackoverflow.com/a/1484514 */
+function randomGradientGenerator() {
+    var letters = '0123456789ABCDEF';
+    var c1 = '#';
+    var c2 = '#';
+
+    for(var i = 0; i < 6; ++i) {
+        c1 += letters[Math.floor(Math.random() * 16)];
+        c2 += letters[Math.floor(Math.random() * 16)];
+    }
+
+    color1.value = c1;
+    color2.value = c2;
+
+    body.style.background = "linear-gradient(to right, "
+        + c1
+        + ", "
+        + c2
+        + ")";
+
+    showProperty();
+}
+
 // Event listeners
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+randomIcon.addEventListener("click", randomGradientGenerator);
