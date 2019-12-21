8be06211584f2c3bea1648f03f4c7fbd44e8bcf2
Code-Plus-Coffee <m416a@protonmail.com>
2019-12-21 12:43:19 +0530

    Fixed few issues.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let cssProperty = document.getElementById("css-prop-display");
 
 // To display the corresponding CSS property
 function showProperty() {
-    cssProperty.textContent = bd.backgroundImage + ";";
+  cssProperty.textContent = bd.backgroundImage + ";";
 }
 
 // To display the current CSS Property on load
@@ -14,10 +14,10 @@ let color2 = document.getElementById("c2");
 
 // Function to set gradient
 function setGradient() {
-    body.style.background =
-        "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
-    showProperty();
+  showProperty();
 }
 
 color1.addEventListener("input", setGradient);
@@ -27,18 +27,18 @@ color2.addEventListener("input", setGradient);
 let playBtn = document.getElementsByClassName("play-icon")[0];
 
 playBtn.addEventListener("click", () => {
-    playBtn.src = pauseBtnSrc;
-    body.style = null;
-    body.classList.toggle("bg-play");
-    color1.value = "#e73c7e";
-    color2.value = "#23a6d5";
+  playBtn.src = pauseBtnSrc;
+  body.style = null;
+  body.classList.toggle("bg-play");
+  color1.value = "#e73c7e";
+  color2.value = "#23a6d5";
 
-    showProperty();
+  showProperty();
 
-    // To toggle back the play button
-    if (body.classList != "bg-play") {
-        playBtn.src = playBtnSrc;
-    }
+  // To toggle back the play button
+  if (body.classList != "bg-play") {
+    playBtn.src = playBtnSrc;
+  }
 });
 
 /* Click to copy function
@@ -46,64 +46,63 @@ playBtn.addEventListener("click", () => {
 let copyButton = document.getElementById("copy");
 
 copyButton.addEventListener("click", () => {
-    let = ifCopySupported = document.queryCommandSupported("copy");
+  let = ifCopySupported = document.queryCommandSupported("copy");
 
-    if (ifCopySupported === true) {
-        let textArea = document.createElement("textarea");
-        textArea.value = cssProperty.textContent
-        document.body.appendChild(textArea);
-        textArea.focus();
-        textArea.select();
+  if (ifCopySupported === true) {
+    let textArea = document.createElement("textarea");
+    textArea.value = cssProperty.textContent;
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
 
-        try {
-            document.execCommand("copy");
-        } catch (e) {
-            console.log("Error!");
-        }
-
-        document.body.removeChild(textArea);
-    } else {
-        window.prompt("CTRL + C, Enter", textArea.value);
+    try {
+      document.execCommand("copy");
+    } catch (e) {
+      console.log("Error!");
     }
+
+    document.body.removeChild(textArea);
+  } else {
+    window.prompt("CTRL + C, Enter", textArea.value);
+  }
 });
 
 /* Function to generate random gradients 
     https://stackoverflow.com/a/1484514 */
 let randomIcon = document.getElementsByClassName("random-icon")[0];
-let pauseBtnSrc =
-    "https://img.icons8.com/material-rounded/48/000000/pause.png";
+let pauseBtnSrc = "https://img.icons8.com/material-rounded/48/000000/pause.png";
 let playBtnSrc =
-    "https://img.icons8.com/material-rounded/90/000000/play--v1.png";
+  "https://img.icons8.com/material-rounded/90/000000/play--v1.png";
 
 randomIcon.addEventListener("click", () => {
-    body.classList = "";
+  body.classList = "";
 
-    // To toggle back the play button
-    if (body.classList != "bg-play") {
-        playBtn.src = playBtnSrc;
-    }
+  // To toggle back the play button
+  if (body.classList != "bg-play") {
+    playBtn.src = playBtnSrc;
+  }
 
-    let letters = "0123456789ABCDEF";
-    let c1 = "#";
-    let c2 = "#";
+  let letters = "0123456789ABCDEF";
+  let c1 = "#";
+  let c2 = "#";
 
-    for (let i = 0; i < 6; ++i) {
-        c1 += letters[Math.floor(Math.random() * 16)];
-        c2 += letters[Math.floor(Math.random() * 16)];
-    }
+  for (let i = 0; i < 6; ++i) {
+    c1 += letters[Math.floor(Math.random() * 16)];
+    c2 += letters[Math.floor(Math.random() * 16)];
+  }
 
-    color1.value = c1;
-    color2.value = c2;
+  color1.value = c1;
+  color2.value = c2;
 
-    body.style.background = "linear-gradient(to right, " + c1 + ", " + c2 + ")";
+  body.style.background = "linear-gradient(to right, " + c1 + ", " + c2 + ")";
 
-    showProperty();
+  showProperty();
 });
 
 // To reset color values to defaults
 window.onbeforeunload = () => {
-    body.style = null;
-    body.class = null;
-    color1.value = "#3478cb";
-    color2.value = "#b929c9";
-}
+  body.style = null;
+  body.class = null;
+  color1.value = "#3478cb";
+  color2.value = "#b929c9";
+};
